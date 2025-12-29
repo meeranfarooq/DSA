82d16596b067bdc8ee21919095bc9ff6cd8cd611
meeranfarooq <meeranbhat24@gmail.com>
2025-12-29 11:36:33 +0530

    Add binary tree and inorder traversal implementation
    Implement binary tree structure and inorder traversal

---
+++ Dsa assignment.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node* newNode(int data) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorder(struct node* root) {
+    struct node* stack[100];
+    int top = -1;
+    struct node* curr = root;
+
+    while (curr != NULL || top != -1) {
+        while (curr != NULL) {
+            stack[++top] = curr;
+            curr = curr->left;
+        }
+
+        curr = stack[top--];
+        printf("%d ", curr->data);
+        curr = curr->right;
+    }
+}
+
+int main() {
+    struct node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    inorder(root);
+    return 0;
+}
